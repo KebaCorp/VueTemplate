eaee20fa9d49c6a699ad49e2d3ca03cc9a91d28d
AbekNarynov <vhkeba@gmail.com>
2019-03-10 16:54:42 +0300

    feat(General): Vue config file added

--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/index'
+import i18n from './i18n/index'
 import './registerServiceWorker'
+import '@/resources/main.sass'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  publicPath: process.env.VUE_APP_PUBLIC_PATH
+  publicPath: process.env.VUE_APP_PUBLIC_PATH,
+
+  pluginOptions: {
+    i18n: {
+      locale: process.env.VUE_APP_I18N_LOCALE || 'en',
+      fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
+      localeDir: 'locales',
+      enableInSFC: false
+    }
+  }
 }
