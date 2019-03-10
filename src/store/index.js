a66114a46d7dbfccc3cf29ec512606ab40e9edda
AbekNarynov <vhkeba@gmail.com>
2019-03-10 16:10:35 +0300

    feat(General): Setting changed

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import example from './modules/example'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  strict: process.env.NODE_ENV !== 'production',
+  modules: {
+    example
+  }
+})
